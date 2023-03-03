d8e461064eecc87292f1d90958ff87e2bd768b89
Rick Alves <henryworkdev.ti@gmail.com>
2023-03-02 21:51:05 -0300

    primeiro commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import Home from './src/views/Home';
+
+function App(){
+  return <SafeAreaView>
+    <Home />
+  </SafeAreaView>
+}
+
+export default App;
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Topo from "./components/Topo";
+
+export default function Home(){
+    return <>
+        <Topo />
+    </>
+}
